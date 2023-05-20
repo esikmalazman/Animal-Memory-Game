60ba86b4aee3307c8e8fb11cd50e6ff2d5563797
Ikmal Azman <opikmalazman@gmail.com>
2023-05-20 23:07:10 +0100

    create custom card collection view cell for card collection view

---
+++ CDW-MemoryMatchCardGame/CardCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CardCollectionViewCell.swift
+//  CDW-MemoryMatchCardGame
+//
+//  Created by Ikmal Azman on 20/05/2023.
+//
+
+import UIKit
+
+final class CardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var frontImageView : UIImageView!
+    @IBOutlet weak var backImageView : UIImageView!
+    
+}
