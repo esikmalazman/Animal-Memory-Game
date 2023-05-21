41113e61554fbd694370fe5425550e47a517762f
Ikmal Azman <opikmalazman@gmail.com>
2023-05-21 23:24:52 +0100

    implement method to generate unique pairs of card and shuffle the position of the elements in array so the same card not place beside each other

--- CDW-MemoryMatchCardGame/Model/CardModel.swift
+++ CDW-MemoryMatchCardGame/Model/CardModel.swift
@@ -10,29 +10,35 @@ import Foundation
 final class CardModel {
     
     func getCards() -> [Card] {
+        // Array to store numbers alread been generated
+        var generatedNumbersArray = [Int]()
+        
         // Array to store generated cards
         var generatedCardsArray = [Card]()
         
         // Randomly generate pairs of cards, in this case we do 8 pairs so we loop 8 times
-        for _ in 1...8 {
+        while generatedNumbersArray.count < 8 {
             // Generate random numbers
             let randomNumbers = Int.random(in: 1...13)
-            print(randomNumbers)
             
-            // Create 2 same card object because we need pair of card to match in the game
-            let cardOne = Card()
-            cardOne.cardName = "card\(randomNumbers)"
-            generatedCardsArray.append(cardOne)
-            
-            let cardTwo = Card()
-            cardTwo.cardName = "card\(randomNumbers)"
-            generatedCardsArray.append(cardTwo)
-            
-            // OPTIONAL : Verify only unique pairs of card generated, to avoid same pairs of card being generated
+            // Make sure generated numbers are not already have. Allow only unique pairs of card generated.
+            if generatedNumbersArray.contains(randomNumbers) == false {
+                
+                generatedNumbersArray.append(randomNumbers)
+                print(randomNumbers)
+                
+                // Create 2 same card object because we need pair of card to match in the game
+                let cardOne = Card()
+                cardOne.cardName = "card\(randomNumbers)"
+                generatedCardsArray.append(cardOne)
+                
+                let cardTwo = Card()
+                cardTwo.cardName = "card\(randomNumbers)"
+                generatedCardsArray.append(cardTwo)
+            }
         }
         
-        // TODO : Randomize the array
-        
-        return generatedCardsArray
+        //  Randomize the array by shuffle the position of elements
+        return generatedCardsArray.shuffled()
     }
 }
