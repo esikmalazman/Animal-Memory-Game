58894ed25a1634dae3538a094a4c48c630dc6058
Ikmal Azman <opikmalazman@gmail.com>
2023-05-21 21:47:00 +0100

    implementation of timer and to verify if the game ended

--- CDW-MemoryMatchCardGame/ViewController.swift
+++ CDW-MemoryMatchCardGame/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class ViewController: UIViewController {
     
+    @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     
     var model = CardModel()
@@ -16,6 +17,14 @@ final class ViewController: UIViewController {
     
     /// Track if the first card already been flipped. If empty it is a first card, else second card.
     var firstFlippedCardIndex : IndexPath?
+    var timer : Timer?
+    /// Time give for user to complete the puzzle
+    /// 10 seconds
+    var miliseconds : Float = 10 * 1000
+    
+    var isThereAnyTimeLeft : Bool {
+        return miliseconds > 0
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +32,36 @@ final class ViewController: UIViewController {
         collectionView.dataSource = self
         
         cardArray = model.getCards()
+        // Create timer with time interval of miliseconds, it is seconds by default
+        timer = Timer.scheduledTimer(timeInterval: 0.001,
+                                     target: self,
+                                     selector: #selector(timeElapsed),
+                                     userInfo: nil,
+                                     repeats: true)
+        // Make the timer run even the user scroll or not, so timer not stop. By default, it will stop because it is on default RunLoop mode
+        RunLoop.main.add(timer!, forMode: .common)
+    }
+    
+    // Method will active everytime timer elapse from 1 miliseconds
+    @objc
+    func timeElapsed() {
+        // Deduct 1 seconds when method active
+        miliseconds = miliseconds - 1
+        
+        // Convert miliseconds into seconds and limit the decimal to 2 decimal places
+        let seconds = String(format: "%.2f", miliseconds/1000)
+        
+        timerLabel.text = "Time Remaining : \(seconds)"
+        
+        // Stop timer when it reach 0 miliseconds
+        if miliseconds <= 0 {
+            timer?.invalidate()
+            timerLabel.textColor = .red
+            collectionView.isUserInteractionEnabled = false
+            
+            // Check if there's other card unmatched
+            checkedGameEnded()
+        }
     }
 }
 
@@ -95,6 +134,9 @@ extension ViewController {
             cardOneCell?.remove()
             cardTwoCell?.remove()
             
+            // Check if there's any card left unmatched
+            checkedGameEnded()
+            
         } else {
             // Cards not Matched
             
@@ -115,5 +157,45 @@ extension ViewController {
         
         // Reset the first card index and allow the matching logic start over again
         self.firstFlippedCardIndex = nil
+    }
+    
+    func checkedGameEnded(){
+        // Check if there any card unmatched
+        var isUserWon : Bool = true
+        
+        // If not , user's won and stop timer
+        /// Iterate item in the array to see if there any umatched card, if there any stop the loop
+        for card in cardArray {
+            if card.isMatched == false {
+                isUserWon = false
+                break
+            }
+        }
+        
+        // If there any unmatched card, check time any left
+        if isUserWon {
+            if isThereAnyTimeLeft {
+                timer?.invalidate()
+            }
+            
+            // Show Won Messages Alert
+            showAlert(title: "Congratulations", message: "You have Won")
+        } else {
+            // Return if there's any time left
+            if isThereAnyTimeLeft {
+                return
+            }
+            
+            // Show Lost Messages Alert
+            showAlert(title: "Game Over", message:  "You have lost")
+        }
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(alertAction)
+        self.present(alert, animated: true)
     }
 }
