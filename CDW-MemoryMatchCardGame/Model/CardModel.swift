f9cd8b3d2ef34b91490f169cb1387b6dc27e3115
Ikmal Azman <opikmalazman@gmail.com>
2023-05-27 07:00:22 +0100

    add animals assets into project

--- CDW-MemoryMatchCardGame/Model/CardModel.swift
+++ CDW-MemoryMatchCardGame/Model/CardModel.swift
@@ -16,10 +16,10 @@ final class CardModel {
         // Array to store generated cards
         var generatedCardsArray = [Card]()
         
-        // Randomly generate pairs of cards, in this case we do 8 pairs so we loop 8 times
-        while generatedNumbersArray.count < 8 {
+        // Randomly generate pairs of cards, in this case we do 5 pairs so we loop 5 times
+        while generatedNumbersArray.count < cardsDataSouce.count {
             // Generate random numbers
-            let randomNumbers = Int.random(in: 1...13)
+            let randomNumbers = Int.random(in: 0...cardsDataSouce.count - 1)
             
             // Make sure generated numbers are not already have. Allow only unique pairs of card generated.
             if generatedNumbersArray.contains(randomNumbers) == false {
@@ -29,16 +29,21 @@ final class CardModel {
                 
                 // Create 2 same card object because we need pair of card to match in the game
                 let cardOne = Card()
-                cardOne.cardName = "card\(randomNumbers)"
+                cardOne.cardName = cardsDataSouce[randomNumbers]
                 generatedCardsArray.append(cardOne)
                 
                 let cardTwo = Card()
-                cardTwo.cardName = "card\(randomNumbers)"
+                cardTwo.cardName = cardsDataSouce[randomNumbers]
                 generatedCardsArray.append(cardTwo)
             }
         }
         
+        #warning("shuffle back when test done")
+        return generatedCardsArray
         //  Randomize the array by shuffle the position of elements
-        return generatedCardsArray.shuffled()
+        //  return generatedCardsArray.shuffled()
     }
 }
+
+
+let cardsDataSouce = ["BlueWhale","HawksbillSeaTurtle", "JavaRhinoceros", "LittleBluePenguin", "PolarBear"]
